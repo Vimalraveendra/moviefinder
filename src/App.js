98cd14a420cdd4cb010f21ee4536e7b5b578a13c
vimal Raveendran <vimalraveendra@gmail.com>
2020-02-24 23:55:51 +0100

    created title folder

--- src/App.js
+++ src/App.js
@@ -1,57 +1,51 @@
-import React,{Component} from 'react';
-import './App.css';
-import Title from './Title';
-import MovieList from './MovieList';
+import React, { Component } from "react";
+import "./App.css";
+import Title from "./title/Title";
+import MovieList from "./MovieList";
 
-const API_KEY =process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-class App extends Component{
-  state={
-    input:'',
-    movies:[],
-    error:''
-  }
+class App extends Component {
+  state = {
+    input: "",
+    movies: [],
+    error: ""
+  };
 
-  onHandleChange = (event)=>{
-    this.setState({input:event.target.value})
-  }
-  
-  onHandleSubmit = ()=>{
-        const {input} = this.state
-        if(input.length > 0){
-        fetch(`http://www.omdbapi.com/?s=${input}&apikey=${API_KEY}`)
-        .then(response=> response.json())
-        .then(data=>this.setState({
-           movies:data.Search
-        }))
-        .catch(err=>(err))
-        this.setState({input:''})
-        
-      }else{
-         this.setState({error:"Sorry!!!, Please enter a film name"})
-      }
-      }
+  onHandleChange = event => {
+    this.setState({ input: event.target.value });
+  };
 
-      
-  render(){
-        return (
-        <div className="App">
-          <header className="App-header">
-           MovieFinder
-          </header>
-          <Title
-          input={this.state.input} 
+  onHandleSubmit = () => {
+    const { input } = this.state;
+    if (input.length > 0) {
+      fetch(`http://www.omdbapi.com/?s=${input}&apikey=${API_KEY}`)
+        .then(response => response.json())
+        .then(data =>
+          this.setState({
+            movies: data.Search
+          })
+        )
+        .catch(err => err);
+      this.setState({ input: "" });
+    } else {
+      this.setState({ error: "Sorry!!!, Please enter a film name" });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">MovieFinder</header>
+        <Title
+          input={this.state.input}
           onHandleChange={this.onHandleChange}
           onHandleSubmit={this.onHandleSubmit}
-          />
-          <MovieList
-          moviesArray={this.state.movies}
-           />
-          
-        </div>
-      );
-      }
-      
-    }
+        />
+        <MovieList moviesArray={this.state.movies} />
+      </div>
+    );
+  }
+}
 
 export default App;
