af82a49c431ffd654f26b3bce9d84c9fa32ce955
vimal Raveendran <vimalraveendra@gmail.com>
2020-02-24 23:57:53 +0100

    modified title styles

--- src/title/Title.js
+++ src/title/Title.js
@@ -1,24 +1,24 @@
-import React from 'react';
-
-const Title = ({onHandleChange,input,onHandleSubmit}) =>{
-	return(
-		<React.Fragment>
-		 <div>
-		 <input 
-		 className='f4 pa2 ma4'
-		 type="text"
-		 value={input}
-         onChange={onHandleChange}
-		 />
-		 <button
-		 className='f4 pv2 ph3 grow bg-dark-green white link'
-		 onClick={onHandleSubmit}
-		 >
-		 Click
-		 </button>
-		 </div>
-		</React.Fragment>
-	)
-}
+import React from "react";
+import "./Title.css";
+const Title = ({ onHandleChange, input, onHandleSubmit }) => {
+  return (
+    <React.Fragment>
+      <div>
+        <input
+          className="f4 pa2 ma4"
+          type="text"
+          value={input}
+          onChange={onHandleChange}
+        />
+        <button
+          className="f4 pv2 ph3 grow bg-dark-green white link"
+          onClick={onHandleSubmit}
+        >
+          Click
+        </button>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Title;
