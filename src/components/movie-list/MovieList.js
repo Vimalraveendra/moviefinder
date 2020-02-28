e1f9be0ab4efed7ba94265caf28236ff55856a7a
vimal Raveendran <vimalraveendra@gmail.com>
2020-02-28 19:36:50 +0100

    created components folder & placed  all the components together

---
+++ src/components/movie-list/MovieList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import MovieDisplay from "../movie-display/MovieDisplay";
+import "./MovieList.css";
+
+const MovieList = ({ moviesArray }) => {
+  return (
+    <React.Fragment>
+      <div className="grid-container">
+        {moviesArray.map(item => {
+          return (
+            <MovieDisplay
+              key={item.imdbID}
+              title={item.Title}
+              poster={item.Poster}
+              year={item.Year}
+            />
+          );
+        })}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default MovieList;
