82df0e9f3fd770526e10f94e14c073ff8870f1ed
vimal Raveendran <vimalraveendra@gmail.com>
2020-05-07 12:17:41 +0200

    Updates

---
+++ precache-manifest.e5ddadd1fc7d2ba6a1e4147dc5da492f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b605648098ec282fc26ab7b8a88f3b",
+    "url": "/moviefinder/index.html"
+  },
+  {
+    "revision": "8132b14b381dfc213179",
+    "url": "/moviefinder/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "dae0b39f0be40d5d0ca3",
+    "url": "/moviefinder/static/css/main.d127b8f6.chunk.css"
+  },
+  {
+    "revision": "8132b14b381dfc213179",
+    "url": "/moviefinder/static/js/2.1c1f0103.chunk.js"
+  },
+  {
+    "revision": "dae0b39f0be40d5d0ca3",
+    "url": "/moviefinder/static/js/main.33ef3e09.chunk.js"
+  },
+  {
+    "revision": "4569bc13586cca0f510c",
+    "url": "/moviefinder/static/js/runtime-main.02090348.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/moviefinder/precache-manifest.e5ddadd1fc7d2ba6a1e4147dc5da492f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moviefinder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
