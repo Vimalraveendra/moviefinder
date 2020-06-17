0e9fadbbe8a52f91c917cb2e9fe07967f7f92c6d
vimal Raveendran <vimalraveendra@gmail.com>
2020-06-17 22:51:21 +0200

    Updates

---
+++ precache-manifest.6cf74306ca0cdfe95992fe8cb84f4ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539ee900661d1d76665d4b0362369abd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8132b14b381dfc213179",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "43ac8e8721bae7b90b83",
+    "url": "/static/css/main.970e9135.chunk.css"
+  },
+  {
+    "revision": "8132b14b381dfc213179",
+    "url": "/static/js/2.1c1f0103.chunk.js"
+  },
+  {
+    "revision": "43ac8e8721bae7b90b83",
+    "url": "/static/js/main.a9c89957.chunk.js"
+  },
+  {
+    "revision": "fe67840a2919da418ace",
+    "url": "/static/js/runtime-main.b9d3bfa0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/moviefinder/precache-manifest.91bb56f5be644d0e02aeb42dbc7645fb.js"
+  "/precache-manifest.6cf74306ca0cdfe95992fe8cb84f4ab8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moviefinder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
